80dae4256e2f54a2dd291f2a567480bf219a777b
elena_ceceva <elena.ceceva@webfactory.mk>
2020-04-07 19:11:07 +0200

    Fix Mailtrap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,6 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
   # `config/secrets.yml.key`.
@@ -77,7 +76,7 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-
+  config.action_mailer.default_url_options = { :host => 'warm-gorge-77467.herokuapp.com' }
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
