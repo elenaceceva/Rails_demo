300001be718ea9a5c17e09ee6d4cc065608b3a4e
elena_ceceva <elena.ceceva@webfactory.mk>
2020-04-07 17:49:39 +0200

    New relic and mailtrap

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'geokit-rails'
 gem "pundit"
 gem 'apipie-rails'
 gem 'kaminari'
+gem 'newrelic_rpm'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :user_name => '007a50894484e7',
+      :password => '60848d2d29a0c9',
+      :address => 'smtp.mailtrap.io',
+      :domain => 'smtp.mailtrap.io',
+      :port => '2525',
+      :authentication => :cram_md5
+  }
 end
