58b2c5eecb87946851948c0b312c08a363f9fa9e
elena_ceceva <elena.ceceva@webfactory.mk>
2020-04-11 21:03:20 +0200

    heroku fix

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -76,6 +76,6 @@ class Api::V1::UsersController < BaseController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :nickname, :firstname, :lastname, picture: [:picture_file_name, :picture_file_size, :picture_content_type, :picture_updated_at], location_attributes: [:country, :city, :latitude, :longitude ])
+    params.require(:user).permit(:email, :password, :nickname, :firstname, :lastname, :picture, location_attributes: [:country, :city, :latitude, :longitude ])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,7 @@ class Post < ApplicationRecord
 
   attr_accessor :location_attributes
   attr_accessor :tag_attributes
+  attr_accessor :picture_attributes
 
   has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
