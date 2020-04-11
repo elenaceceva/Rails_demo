f7e3869c8b7d9a0f143193b096cba7667b20d1d6
elena_ceceva <elena.ceceva@webfactory.mk>
2020-04-12 00:12:18 +0200

    s3 storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,10 +92,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
       storage: :s3,
       s3_credentials: {
-          bucket: ENV['rails-demo-app-bucket'],
-          access_key_id: ENV['AKIAIKDRPYYTNVN4SA3A'],
-          secret_access_key: ENV['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'],
-          s3_region: ENV['us-east-2'],
+          bucket: ENV["S3_BUCKET_NAME"],
+          access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+          secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+          s3_region: ENV["S3_REGION"]
       }
   }
 end
